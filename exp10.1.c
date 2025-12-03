1491b85fccd221ca1a1e26e0b4cb08f50aea9a68
ishita bora <ishitabora20@gmail.com>
2025-12-03 21:51:56 +0530

    Add files via upload

---
+++ exp10.1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+int main() {
+    struct Node *head = NULL, *temp, *newnode;
+    int n, value;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        newnode = (struct Node*)malloc(sizeof(struct Node));
+        printf("Enter value: ");
+        scanf("%d", &value);
+
+        newnode->data = value;
+        newnode->next = NULL;
+
+        if (head == NULL) {
+            head = newnode;
+        } else {
+            temp = head;
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = newnode;
+        }
+    }
+
+    printf("Linked List: ");
+    temp = head;
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
